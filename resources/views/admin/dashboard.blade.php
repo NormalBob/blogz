8e5b34084365a41a559587b47cf2bb5a8c641971
NormalBob <dima.dagar@gmail.com>
2019-02-17 12:39:27 +0200

    Create admin dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,50 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Categories 0</span></p>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Materials 0</span></p>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Users 0</span></p>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Users Today 0</span></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#">Create category</a>
+                <a class="list-group-item" href="#">
+                    <h4 class="list-group-item-heading">First category</h4>
+                    <p class="list-group-item-text">Count of materials</p>
+                </a>
+            </div>
+
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#">Create material</a>
+                <a class="list-group-item" href="#">
+                    <h4 class="list-group-item-heading">First material</h4>
+                    <p class="list-group-item-text">Category</p>
+                </a>
+            </div>
+        </div>
+
+    </div>
+@endsection
